5c22c81723746f22b13cb819694fd894e21bb83b
egosteva <elenagosteva.qa@gmail.com>
2023-04-09 15:33:24 +0300

    add lambda steps

--- src/test/java/com/github/egosteva/tests/TestBase.java
+++ src/test/java/com/github/egosteva/tests/TestBase.java
@@ -1,7 +1,10 @@
 package com.github.egosteva.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -20,5 +23,10 @@ public class TestBase {
                         "enableVNC", true,
                         "enableVideo", true
                 ));
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 }
