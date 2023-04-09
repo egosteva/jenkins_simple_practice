5c4561975f527fe0c73b24d174559094cbfbc418
egosteva <elenagosteva.qa@gmail.com>
2023-04-09 16:29:08 +0300

    add video attach

--- src/test/java/com/github/egosteva/tests/TestBase.java
+++ src/test/java/com/github/egosteva/tests/TestBase.java
@@ -37,5 +37,6 @@ public class TestBase {
         Attach.screenshotAs("Last step screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
